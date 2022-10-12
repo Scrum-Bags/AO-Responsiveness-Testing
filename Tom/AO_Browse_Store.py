109fe911b7de5c5285e08db7df63cae82c731295
Tom Hennessey <tomahennessey@gmail.com>
2022-10-12 16:16:27 -0400

    Changed edge option import to chrome

--- Tom/AO_Browse_Store.py
+++ Tom/AO_Browse_Store.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By 
-from selenium.webdriver.edge.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 import time, random, unittest, logging
 
