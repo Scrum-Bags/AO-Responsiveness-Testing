6b960c8a4dbcd0b3463e90bf72febaea89636178
Tom Hennessey <tomahennessey@gmail.com>
2022-10-12 14:24:42 -0400

    Fixes to chromedriver path

--- Tom/AO_Browse_Store.py
+++ Tom/AO_Browse_Store.py
@@ -32,8 +32,8 @@ class AO_Browse_Store(unittest.TestCase):
         cls.chrome_options = Options()
         cls.chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
         cls.chrome_options.headless = True
-        cls.chrome_options.binary_location = "/usr/bin/chromedriver"
-        cls.driver = webdriver.Chrome(options=cls.chrome_options)
+        #cls.chrome_options.binary_location = "/usr/bin/chromedriver"
+        cls.driver = webdriver.Chrome(options=cls.chrome_options, executable_path='/usr/bin/chromedriver')
         cls.driver.loggingID = "AO_Browse_Store"
 
     @classmethod
