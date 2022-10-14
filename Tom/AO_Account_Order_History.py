dd85626543afee9295fa0525695afb778cad02eb
Tom Hennessey <tomahennessey@gmail.com>
2022-10-14 16:03:28 -0400

    Fixed chrome options import

--- Tom/AO_Account_Order_History.py
+++ Tom/AO_Account_Order_History.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.chrome import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 import time, random, unittest, logging
