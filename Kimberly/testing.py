722b5affc7dae1d625021afdbb54e92e830c49fd
Kimberly Modeste <Kimberly.Modeste@smoothstack.com>
2022-10-17 13:53:36 -0400

    Renamed jenkins folder to Test

--- Kimberly/testing.py
+++ Kimberly/testing.py
@@ -196,9 +196,24 @@
 # path = path.replace('\\', '/')
 # print(path)
 
-foo = []
-bar = ["James", "Came", "Don", "Phil"]
+# foo = []
+# bar = ["James", "Came", "Don", "Phil"]
 
-for i in range(5):
-    foo.append(i)
-print(f"{str(bar)[1:-2]}")
+# for i in range(5):
+#     foo.append(i)
+# print(f"{str(bar)[1:-2]}")
+
+arr = [1, 2, 3, 6, 5, 4, 4, 2, 5, 3, 6, 1, 6, 5, 3, 2, 4, 1, 2, 5, 1, 4, 3, 6, 8, 4, 3, 1, 5, 6, 2]
+s = set(arr)
+
+
+
+for i in range(len(s)):
+    x = s.pop()
+    if arr.count(x) < 5:
+        print(x)
+
+# for i in range(len(arr)):
+#     if arr.count(arr[i]) == 1:
+#         print(arr[i])
+
